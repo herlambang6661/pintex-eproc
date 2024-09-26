7489083c2fd0ef8bb938f7342eeb60de58a2fe92
Yudha <herlambang6661@gmail.com>
2024-09-26 10:07:09 +0700

    add edit in servis

--- app/Http/Controllers/Datatables/Teknik/ServisList.php
+++ app/Http/Controllers/Datatables/Teknik/ServisList.php
@@ -82,6 +82,11 @@ class ServisList extends Controller
                                         <svg style="margin-right:5px;" xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="1.5"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-edit"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" /><path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" /><path d="M16 5l3 3" /></svg>
                                         Edit
                                     </a>';
+                    } elseif (Auth::user()->entitas_all == 1) {
+                        $btnEdit = '<a href="#" class="dropdown-item edit-btn" data-bs-toggle="modal" data-bs-target="#modalEditServis" data-id="' . $row->id . '">
+                                        <svg style="margin-right:5px;" xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="1.5"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-edit"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" /><path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" /><path d="M16 5l3 3" /></svg>
+                                        Edit
+                                    </a>';
                     } else {
                         $btnEdit = ' ';
                     }
