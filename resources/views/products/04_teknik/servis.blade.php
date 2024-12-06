dfc3c67a24aa4c9cc1de88142c7c0b631b1e8ec1
arizki07 <116191090+arizki07@users.noreply.github.com>
2024-12-06 13:33:31 +0700

    servis

--- resources/views/products/04_teknik/servis.blade.php
+++ resources/views/products/04_teknik/servis.blade.php
@@ -185,6 +185,8 @@
                                                     <th class="px-1 th py-1">noform</th>
                                                     <th class="px-1 th py-1">tgl</th>
                                                     <th class="px-1 th py-1">barang</th>
+                                                    <th class="px-1 th py-1">mesin</th>
+                                                    <th class="px-1 th py-1">kdoe</th>
                                                     <th class="px-1 th py-1">deskripsi</th>
                                                     <th class="px-1 th py-1">serialnumber</th>
                                                     <th class="px-1 th py-1">qty</th>
